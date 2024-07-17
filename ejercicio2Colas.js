96fb44def7e8979fc4c71c784a43eeee4a948a8b
Javier Cornejo <javicg0711@gmail.com>
2024-07-16 23:44:27 -0600

    Termino de ejercicio colas 2

--- ejercicio2Colas.js
+++ ejercicio2Colas.js
@@ -20,3 +20,65 @@ Cola: [
 ];
 
 */
+
+console.log("===================  EJERCICIO COLAS 2 ==============");
+
+let cola = [
+            { user:"User1", ticket:true },
+            { user:"User2", ticket:true },
+            { user:"User3", ticket:false },
+            { user:"User4", ticket:true },
+            { user:"User5", ticket:false },
+            { user:"User6", ticket:false },
+            { user:"User7", ticket:true },
+            { user:"User8", ticket:true },
+            { user:"User9", ticket:true },
+            { user:"User10", ticket:false },
+            { user:"User11", ticket:true },
+];
+
+console.log(" ==========  La cola INICIAL es:  =============");
+console.log(cola);
+let personasRetiradas = [];
+let retirando;
+let colaFinal = [];
+
+function Retirando(user, ticket){
+    this.user = user;
+    this.ticket = ticket;
+}
+
+function Reformando(user, ticket){
+    this.user = user;
+    this.ticket = ticket;
+}
+
+
+for(let i = 0; i < cola.length; i ++){
+
+    if(cola[i].ticket == false){
+
+        console.log(`Se identificó que la persona: ${cola[i].user} ubicada en la posicion ${i} NO tiene boleto, se pasa a la cola de personas retiradas`);
+
+        retirando = new Retirando(cola[i].user,cola[i].ticket);
+
+        console.log(retirando);
+
+        personasRetiradas.push(retirando);
+
+        
+        
+    }
+    else{
+
+        reformando = new Reformando(cola[i].user, cola[i].ticket)
+        colaFinal.push(reformando);
+    }
+
+}
+
+
+console.log(" ==========  La cola de personas retiradas es: ===========  ");
+console.log(personasRetiradas);
+console.log(" ==========  La cola final quedó:  =============");
+console.log(colaFinal);
